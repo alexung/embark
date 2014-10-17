13f4a1b29db543447a1cf9526d16eca4dcd9de22
Alex Ung <alexung.ucsb@gmail.com>
2014-10-17 13:55:39 -0400

    Populate the models/migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :driver_rides, class_name: "Rides", foreign_key: :driver_id
   has_many :passenger_rides, foreign_key: :passenger_id
   has_many :rides, through: :passenger_rides
+  has_many :requests
 end
