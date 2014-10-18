a4743169d4b264523cd9fbc7358627a36cb6b656
Faraaz Nishtar <faraaznishtar@gmail.com>
2014-10-18 15:41:24 -0400

    reafctor message routes into message file in controller

--- app/controllers/message.rb
+++ app/controllers/message.rb
@@ -1,32 +1,3 @@
-get "/ride" do
-  @rides = Ride.all
-  erb :ride_homepage
-end
-
-get '/ride/new' do
-  erb :new_ride
-end
-
-post '/ride/new' do
-  @ride = params[:ride]
-  Ride.create(@ride)
-  redirect '/ride'
-end
-
-get '/ride/success' do
-  erb :ride_requested
-end
-
-post '/ride/success' do
-  Request.create(requestor_id: session[:user_id], requested_ride_id: params[:id], description: params[:description])
-  redirect '/ride/success'
-end
-
-get '/ride/:id' do
-  @ride = Ride.find(params[:id])
-  erb :ride
-end
-
 get '/message' do
   @all_requests = get_requests_for_driver(session[:user_id])
   erb :message
@@ -52,5 +23,3 @@ post '/message/:id' do
   else
     redirect '/ride'
   end
-
-end
